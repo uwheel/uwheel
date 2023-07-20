32caa73416c2f4246f9a0a0bc9f2e94731acc13f
Max Meldrum <max@meldrum.se>
2023-07-20 12:16:56 +0200

    Add examples (closes #45)

---
+++ examples/top-n/src/main.rs
@@ -0,0 +1,47 @@
+use awheel::{
+    aggregator::{
+        top_n::{TopNAggregator, TopNState},
+        U64SumAggregator,
+    },
+    time::NumericalDuration,
+    Entry,
+    RwWheel,
+};
+
+const N: usize = 10;
+
+/// A Top 10 aggregator with u32 as key and U64SumAggregator as inner aggregator
+fn main() {
+    let mut wheel: RwWheel<TopNAggregator<u32, N, U64SumAggregator>> = RwWheel::new(0);
+
+    wheel.write().insert(Entry::new((1u32, 10), 1000)).unwrap();
+    wheel.write().insert(Entry::new((2u32, 50), 1000)).unwrap();
+    wheel.write().insert(Entry::new((3u32, 30), 1000)).unwrap();
+
+    wheel.advance_to(2000);
+
+    let state: TopNState<u32, N, U64SumAggregator> = wheel.read().interval(1.seconds()).unwrap();
+    let arr = state.iter();
+    println!("{:#?}", arr);
+
+    // insert same keys with different values at different timestamp
+    wheel.write().insert(Entry::new((1u32, 100), 2000)).unwrap();
+    wheel.write().insert(Entry::new((2u32, 10), 2000)).unwrap();
+    wheel.write().insert(Entry::new((3u32, 20), 2000)).unwrap();
+
+    // insert some new keys
+    wheel.write().insert(Entry::new((4u32, 15), 2000)).unwrap();
+    wheel.write().insert(Entry::new((5u32, 1), 2000)).unwrap();
+    wheel
+        .write()
+        .insert(Entry::new((6u32, 5000), 2000))
+        .unwrap();
+
+    // advance again
+    wheel.advance_to(3000);
+
+    // interval of last 2 seconds should be two TopKState's combined
+    let state: TopNState<u32, N, U64SumAggregator> = wheel.read().interval(2.seconds()).unwrap();
+    let arr = state.iter();
+    println!("{:#?}", arr);
+}
---
+++ examples/window/src/main.rs
@@ -0,0 +1,20 @@
+use awheel::{
+    aggregator::U64SumAggregator,
+    time::NumericalDuration,
+    window::{
+        eager::{Builder, EagerWindowWheel},
+        *,
+    },
+    Entry,
+};
+fn main() {
+    let mut window: EagerWindowWheel<U64SumAggregator> = Builder::default()
+        .with_range(10.seconds())
+        .with_slide(3.seconds())
+        .build();
+    for i in 1..=22 {
+        window.insert(Entry::new(i, i * 1000 - 1)).unwrap();
+    }
+    let results = window.advance(22.seconds());
+    println!("{:#?}", results);
+}
